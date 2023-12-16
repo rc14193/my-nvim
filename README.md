345b85074ac8b2faaf182303e7dac3a1a22dbcfc
rc14193 <emdort@gmail.com>
2023-12-16 14:19:52 -0500

    bufferline nav

---
+++ README.md
@@ -0,0 +1,4 @@
+My nvim config, mostly pulled from the following with the tweaks I wanted
+
+- https://github.com/LunarVim/Neovim-from-scratch
+- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -79,3 +79,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Navigate buffers
 keymap("n", "<leader>.", ":bnext<CR>", opts)
 keymap("n", "<leader>,", ":bprevious<CR>", opts)
+keymap("n", "<leader>q", ":bd<CR>", opts)
